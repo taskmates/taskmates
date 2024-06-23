a4b37a8a945e04532e1d7fd89b753da79397a837
srizzo <rizzolabs@gmail.com>
2024-06-23 12:10:05 +0200

    refactor

--- taskmates/grammar/parsers/messages_parser.py
+++ taskmates/grammar/parsers/messages_parser.py
@@ -2,11 +2,54 @@ import textwrap
 
 import pyparsing as pp
 
-from taskmates.grammar.parsers.message.message_parser import message_parser, first_message_parser
+from taskmates.grammar.parsers.message.header.chat_message_header_parser import chat_message_header_parser
+from taskmates.grammar.parsers.message.header.code_cell_execution_header_parser import code_cell_execution_header_parser
+from taskmates.grammar.parsers.message.header.tool_execution_header_parser import tool_execution_header_parser
+from taskmates.grammar.parsers.message.tool_calls_parser import tool_calls_parser
 
 pp.enable_all_warnings()
 
 header_delimiter = pp.Suppress(pp.Literal("**"))
+
+message_entry = pp.Forward()
+
+message_header = chat_message_header_parser()
+tool_execution_header = tool_execution_header_parser()
+code_cell_execution_header = code_cell_execution_header_parser()
+message_tool_calls = tool_calls_parser()
+headers = (message_header | tool_execution_header | code_cell_execution_header)
+
+message_content = pp.SkipTo(message_tool_calls | message_entry | pp.stringEnd, include=False).leave_whitespace()(
+    "content")
+
+message_entry <<= pp.Group(
+    headers
+    + message_content
+    + pp.Optional(message_tool_calls))
+
+
+def first_message_parser():
+    global message_entry
+
+    message_content = pp.SkipTo(message_tool_calls | message_entry | pp.stringEnd, include=False).leave_whitespace()(
+        "content")
+    implicit_message_header = (pp.line_start
+                               + pp.Empty().setParseAction(lambda: "user")("name")
+                               )
+    headers = (message_header | tool_execution_header | code_cell_execution_header | implicit_message_header)
+
+    first_message = pp.Group(
+        headers
+        + message_content
+        + pp.Optional(message_tool_calls))
+
+    return first_message
+
+
+def message_parser():
+    global message_entry
+
+    return message_entry
 
 
 def messages_parser():
