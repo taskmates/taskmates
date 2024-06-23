003cce228dcfea01b05e35a6fbac443dde6abcc4
srizzo <rizzolabs@gmail.com>
2024-06-23 17:49:34 +0200

    Speed up headers via Regex anchor

--- taskmates/grammar/parsers/header/headers.py
+++ taskmates/grammar/parsers/header/headers.py
@@ -1,3 +1,7 @@
+import re
+
+import pyparsing as pp
+
 from taskmates.grammar.parsers.message.header.chat_message_header_parser import chat_message_header_parser
 from taskmates.grammar.parsers.message.header.code_cell_execution_header_parser import code_cell_execution_header_parser
 from taskmates.grammar.parsers.message.header.tool_execution_header_parser import tool_execution_header_parser
@@ -7,7 +11,7 @@ def headers_parser():
     message_header = chat_message_header_parser()
     tool_execution_header = tool_execution_header_parser()
     code_cell_execution_header = code_cell_execution_header_parser()
-    return (
+    return pp.Regex(r"^(?=(\*\*|###### ))", re.MULTILINE) + (
             message_header
             | tool_execution_header
             | code_cell_execution_header
