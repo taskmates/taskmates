b97e9ac92c7b3d926b22ea865d5216a0aaae8fe9
srizzo <rizzolabs@gmail.com>
2024-06-23 15:00:17 +0200

    Refactoring

---
+++ taskmates/grammar/parsers/header/headers.py
@@ -0,0 +1,10 @@
+from taskmates.grammar.parsers.message.header.chat_message_header_parser import chat_message_header_parser
+from taskmates.grammar.parsers.message.header.code_cell_execution_header_parser import code_cell_execution_header_parser
+from taskmates.grammar.parsers.message.header.tool_execution_header_parser import tool_execution_header_parser
+
+
+def headers_parser():
+    message_header = chat_message_header_parser()
+    tool_execution_header = tool_execution_header_parser()
+    code_cell_execution_header = code_cell_execution_header_parser()
+    return (message_header | tool_execution_header | code_cell_execution_header)
--- taskmates/grammar/parsers/messages_parser.py
+++ taskmates/grammar/parsers/messages_parser.py
@@ -2,23 +2,17 @@ import textwrap
 
 import pyparsing as pp
 
-from taskmates.grammar.parsers.message.header.chat_message_header_parser import chat_message_header_parser
-from taskmates.grammar.parsers.message.header.code_cell_execution_header_parser import code_cell_execution_header_parser
-from taskmates.grammar.parsers.message.header.tool_execution_header_parser import tool_execution_header_parser
+from taskmates.grammar.parsers.header.headers import headers_parser
 from taskmates.grammar.parsers.message.tool_calls_parser import tool_calls_parser
 
 pp.enable_all_warnings()
 pp.ParserElement.set_default_whitespace_chars("")
 
-header_delimiter = pp.Suppress(pp.Literal("**"))
-
 message_entry = pp.Forward()
 
-message_header = chat_message_header_parser()
-tool_execution_header = tool_execution_header_parser()
-code_cell_execution_header = code_cell_execution_header_parser()
+headers = headers_parser()
+
 message_tool_calls = tool_calls_parser()
-headers = (message_header | tool_execution_header | code_cell_execution_header)
 
 message_content = (pp.SkipTo(message_tool_calls | message_entry | pp.stringEnd, include=False)("content"))
 
