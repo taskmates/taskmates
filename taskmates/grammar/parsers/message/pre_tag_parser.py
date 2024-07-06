e9f6adfb92c1ce2a74941c9c9636f428718aaf21
srizzo <rizzolabs@gmail.com>
2024-07-06 11:14:28 +0200

    Correct <pre> parsing

--- taskmates/grammar/parsers/message/pre_tag_parser.py
+++ taskmates/grammar/parsers/message/pre_tag_parser.py
@@ -4,6 +4,6 @@ import pyparsing as pp
 def pre_tag_parser():
     pre_tag = pp.Combine(
         (pp.LineStart() + pp.Literal("<pre")
-         - pp.SkipTo(pp.LineStart() + pp.Literal("</pre>") + pp.LineEnd(), include=True))
+         - pp.SkipTo(pp.Literal("</pre>") + pp.Optional(pp.LineEnd()), include=True))
     )
     return pre_tag
