4f792cc7c9a69d558ed0e6d7d070d3c72c727865
Samuel Rizzo <samuel365@gmail.com>
2025-01-11 13:29:18 +0300

    Add code_cell_parser tests

--- taskmates/grammar/parsers/message/code_cell_parser.py
+++ taskmates/grammar/parsers/message/code_cell_parser.py
@@ -1,4 +1,5 @@
 import re
+import textwrap
 
 import pyparsing as pp
 
@@ -37,3 +38,100 @@ def code_cell_parser():
     )
 
     return code_cell_with_language | code_cell_without_language
+
+
+def test_code_cell_with_language():
+    input = textwrap.dedent("""\
+        ```python
+        def hello():
+            print("Hello, World!")
+        ```
+        """)
+
+    result = code_cell_parser().parseString(input)[0]
+    assert result == input
+
+
+def test_code_cell_with_language_and_eval():
+    input = textwrap.dedent("""\
+        ```python .eval
+        print("Hello, World!")
+        ```
+        """)
+
+    result = code_cell_parser().parseString(input)[0]
+    assert result == input
+
+
+def test_code_cell_without_language():
+    input = textwrap.dedent("""\
+        ```
+        Some text without language specification
+        ```
+        """)
+
+    result = code_cell_parser().parseString(input)[0]
+    assert result == input
+
+
+def test_nested_code_cells():
+    input = textwrap.dedent("""\
+        ```markdown
+        Here's a code block:
+        ```python
+        print("hello")
+        ```
+        ```
+        """)
+
+    result = code_cell_parser().parseString(input)[0]
+    assert result == input
+
+
+def test_code_cell_with_multiple_lines():
+    input = textwrap.dedent("""\
+        ```python
+        def hello():
+            print("Hello")
+            
+        def world():
+            print("World")
+        ```
+        """)
+
+    result = code_cell_parser().parseString(input)[0]
+    assert result == input
+
+
+def test_code_cell_with_no_closing_backticks():
+    input = textwrap.dedent("""\
+        ```python
+        def hello():
+            print("Hello")
+        """)
+
+    result = code_cell_parser().parseString(input)[0]
+    assert result == input
+
+
+# TODO unsupported
+# def test_code_cell_with_hyphenated_language():
+#     input = textwrap.dedent("""\
+#         ```shell-script
+#         echo "Hello"
+#         ```
+#         """)
+#
+#     result = code_cell_parser().parseString(input)[0]
+#     assert result == input
+
+
+def test_code_cell_with_numbers_in_language():
+    input = textwrap.dedent("""\
+        ```python3
+        print("Hello")
+        ```
+        """)
+
+    result = code_cell_parser().parseString(input)[0]
+    assert result == input
